9f645990d858ac193d76ddabb5c4965dd5875e9d
chris <christopher.pfaeskillsup@gmail.com>
2022-09-28 11:39:39 +0100

    cambios en los php

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -1,5 +1,51 @@
-<?php require ".../require/config.php;
+<?php 
+require "../require/config.php";
 
-$name = $email = $phone = $street = $city = $province = $zip = $newscheck = $news = $other = "";
+$name = $email = $phone = $street = $city = $province = $zip = $newscheck = $news = $other ="";
 
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
+        echo"<br><strong>Método post hay datos</strong><br>";
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $address = $_POST["address"];
+        $city = $_POST["city"];
+        $province = $_POST["province"];
+        $zip = $_POST["zip"];
+        $newscheck = $_POST["newscheck"];
+        $news = $_POST["news"];
+        $other = $_POST["other"];
+
+        function limpiar_dato($data){
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+        }
+
+        function validar_name($name){
+            if(!preg_match("/^[a-zA-Z-' ]$/",$name)){
+                return false;
+            }else{
+                return true;
+            }
+        }
+        function validar_email($email){
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return false;
+            }else{
+                return true;
+            }
+        }
+        function validar_movil($phone){
+            if (!preg_match("(+34|0034|34)?[ -](6|7)[ -]([0-9][ -]*){8}",$phone)) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    } 
+}
 ?>
--- modules/require/config.php
+++ modules/require/config.php
@@ -1,4 +1,14 @@
 <?php
-$name = $email = $phone = $street = $city = $province = $zip = $newscheck = $news = $other = "";
+$servername = "localhost";
+$username = "root";
+$password = "";
 
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myBD", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully<br>";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage() . "<br>";
+}
 ?>
