b1cff2313668e2578b4803b24eba6d4e4208d1e8
chris <christopher.pfaeskillsup@gmail.com>
2022-09-29 11:48:04 +0100

    realizado dia 29 de septiembre

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -3,27 +3,39 @@ require "../require/config.php";
 
 $name = $email = $phone = $street = $city = $province = $zip = $newscheck = $news = $other ="";
 
-
+function limpiar_datos($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+    
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    print_r($_POST);
     if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
         echo"<br><strong>Método post hay datos</strong><br>";
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $phone = $_POST["phone"];
-        $address = $_POST["address"];
-        $city = $_POST["city"];
-        $province = $_POST["province"];
-        $zip = $_POST["zip"];
-        $newscheck = $_POST["newscheck"];
-        $news = $_POST["news"];
-        $other = $_POST["other"];
-
-        function limpiar_dato($data){
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-        }
-
+        $name = limpiar_datos($_POST["name"]);
+        echo " $name ";
+        $email = limpiar_datos($_POST["email"]);
+        echo "$email";
+        $phone = limpiar_datos ($_POST["phone"]);
+        echo "$phone";
+        $address = limpiar_datos ($_POST["address"]);
+        $city = limpiar_datos ($_POST["city"]);
+        $province = limpiar_datos ($_POST["province"]);
+        $zip = limpiar_datos ($_POST["zip"]);
+        $newscheck = limpiar_datos ($_POST["newscheck"]);
+        $news = limpiar_datos ($_POST["news"]);
+        $other = limpiar_datos ($_POST["other"]);
+        var_dump($name);
+    
+    //no funciona
+    if(validar_name($name)){
+        echo"validada";
+    }else{
+        echo"no validada";
+    }
+        
         function validar_name($name){
             if(!preg_match("/^[a-zA-Z-' ]$/",$name)){
                 return false;
