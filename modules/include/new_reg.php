3616c67b3ebe41a438ee197c5c4e3ea8086a83ca
chris <christopher.pfaeskillsup@gmail.com>
2022-09-30 09:38:13 +0100

    actualizacion dia 30 php

--- modules/include/new_reg.php
+++ modules/include/new_reg.php
@@ -1,63 +1,167 @@
-<?php 
+<?php
 require "../require/config.php";
 
-$name = $email = $phone = $street = $city = $province = $zip = $newscheck = $news = $other ="";
+//define y inicializa las variables que se van a usar del formulario.
+$name = $email = $phone = $address = $city = $province = $zip = $other = $news = $newscheck="";
+/**
+ * Función para limpiar un dato procedente de un formulario.
+ * 
+ * @param $data
+ * @return $data
+ */
 
-function limpiar_datos($data){
+function limpiar_dato($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
+
 }
-    
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    print_r($_POST);
-    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
-        echo"<br><strong>Método post hay datos</strong><br>";
-        $name = limpiar_datos($_POST["name"]);
-        echo " $name ";
-        $email = limpiar_datos($_POST["email"]);
-        echo "$email";
-        $phone = limpiar_datos ($_POST["phone"]);
-        echo "$phone";
-        $address = limpiar_datos ($_POST["address"]);
-        $city = limpiar_datos ($_POST["city"]);
-        $province = limpiar_datos ($_POST["province"]);
-        $zip = limpiar_datos ($_POST["zip"]);
-        $newscheck = limpiar_datos ($_POST["newscheck"]);
-        $news = limpiar_datos ($_POST["news"]);
-        $other = limpiar_datos ($_POST["other"]);
-        var_dump($name);
-    
-    //no funciona
-    if(validar_name($name)){
-        echo"validada";
-    }else{
-        echo"no validada";
+
+//nombre, email y nº de telefono.
+/**
+ * Función para validar nombre que solo contenga letras min y MAY, y espacio en blanco.
+ *
+ * @param $name
+ * @return boolean
+ */
+
+    function validar_name($name)
+    {
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+            return false;
+            }else{
+                return true;
     }
-        
-        function validar_name($name){
-            if(!preg_match("/^[a-zA-Z-' ]$/",$name)){
-                return false;
-            }else{
-                return true;
-            }
-        }
-        function validar_email($email){
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                return false;
-            }else{
-                return true;
-            }
-        }
-        function validar_movil($phone){
-            if (!preg_match("(+34|0034|34)?[ -](6|7)[ -]([0-9][ -]*){8}",$phone)) {
-                return false;
-            }
-            else {
-                return true;
-            }
-        }
-    } 
 }
-?>
+
+    function validar_email($email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return false;
+            }else{
+                return true;
+                }
+        }
+        
+        //TODO:documentar función.
+        /**
+         * Validar un número de telefono.
+         * @param $phone
+         * @return Boolean
+        */
+
+        function validar_phone($phone){
+            if(!preg_match('(/^[0-9]+$/)',$phone)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    
+
+//Si (llega datos)Entonces
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    print_r ($_POST);
+    
+    if(!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])){
+    echo "<br><strong>name post hay datos</strong><br>";
+    //Variables requeridas para enviar a BBDD:name,email y phone.
+    $name = limpiar_dato($_POST["name"]);
+    echo"$name <br>";
+    $email = limpiar_dato($_POST["email"]);
+    echo"$email <br>";
+    $phone = limpiar_dato($_POST["phone"]);
+    echo"$phone<br>";
+
+    if(isset($_POST["address"])){
+        $address = limpiar_dato ($_POST["address"]);
+    }else{
+        $address = NULL;
+    }
+    if(isset($_POST["city"])){
+        $city = limpiar_dato ($_POST["city"]);
+    }else{
+        $city = NULL;
+    }
+    if(isset($_POST["province"])){
+        $province = limpiar_dato ($_POST["province"]);
+    }else{
+        $province = NULL;
+    }
+    if(isset($_POST["zip"])){
+        $zip = limpiar_dato ($_POST["zip"]);
+    }else{
+        $zip = NULL;
+    }
+    if(isset($_POST["newscheck"])){
+        $newscheck = limpiar_dato ($_POST["newscheck"]);
+    }else{
+        $newscheck = NULL;
+    }
+    if(isset($_POST["news"])){
+        $news = limpiar_dato ($_POST["news"]);
+    }else{
+        $news = NULL;
+    }
+    if(isset($_POST["other"])){
+        $other = limpiar_dato ($_POST["other"]);
+    }else{
+        $other = NULL;
+    }
+
+
+            //var_dump($newsletter);
+        //echo "</br>";
+        /*          $newsletter = filter_input(
+                INPUT_POST,
+                'newsletter',
+                FILTER_SANITIZE_SPECIAL_CHARS,
+                FILTER_REQUIRE_ARRAY
+            ); */
+        var_dump($newscheck);
+        // echo "</br>";
+
+        // === Usa un array y muestra sus valores separados por coma (o lo que se ponga entre comillas).
+        /*  $string=implode(", ",$newsletter);
+            echo $string;
+            echo "</br>"; */
+        // === FIN MOSTRAR valores array.
+
+        $other = limpiar_dato($_POST["other"]);
+        echo "<strong>Noticias que quiere recibir: $newscheck";
+        var_dump($name);
+
+        echo "<br><strong>Name:</strong> $name <br>";
+        echo "<strong>Telefono:</strong> $phone <br>";
+        echo "<strong>Email: </strong> $email <br>";
+
+        if (validar_name($name)){
+            echo "validada";
+        } else {
+            echo "no valida";
+        };
+
+    }
+}
+
+/* Si (llega datos) Entonces
+    tratamos datos
+        Si si hay información Entonces
+        Si no llegan variables?**
+                limpiar la información. check!!
+                validar la informacinon.
+                Si datos necesarios Entonces
+                    asegurar de que están bien escrito.
+                SiNo
+                    mandamos dato tal cual.
+                Fin Si
+                Mostrar que todos los datos son correctos para enviar a BBDD.
+        SiNo
+            enviar datos necesarios
+        Fin Si
+SiNo
+    avisar no han llegado.
+Fin Si */
